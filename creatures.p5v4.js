e1e3aef3888bd58fe98e21e9d0fa8946a0385b6d
Karl-Heinz Müller <karlheinz@gmail.com>
2023-02-06 22:41:54 -0500

    Added 36 different models

--- creatures.p5v4.js
+++ creatures.p5v4.js
@@ -3,6 +3,7 @@
 // Author: Karl-Heinz Müller
 
 let world;
+let model_details = [];
 
 // the world
 let width = 96;  // 768 px
@@ -13,8 +14,10 @@ let steps = 8;
 let fitness_mit = 140; // fitness cap level for mitosis
 
 // the matches
-let distance = 90;   // Max distance of two genes considered similar/opposite
-let mut_range = 45;  // Max range of possible mutation away from original value
+let distance = 4;   // Max distance of two genes considered similar/opposite
+let mut_range = 128;  // Max range of possible mutation away from original value
+
+
 
 function setup() {
     let canvas = createCanvas(width*steps, height*steps);
@@ -22,10 +25,11 @@ function setup() {
     startWorld();
 
     // Add sliders to control while running
-    models = createSlider(1, 4, 1);
+    models = createSlider(0, 35, 3);
     models.parent('WOCModels');
     pols = createSlider(0, 1, 0); 
     pols.parent('WOCPols');   
+ 
 }
 
 function startWorld() {
@@ -36,17 +40,23 @@ function draw() {
     let WOCModel = document.getElementById("WOCModel");
     let WOCPol = document.getElementById("WOCPol");
     let WOCCreatures = document.getElementById("WOCCreatures");
+    let WOCCalc = document.getElementById("WOCCalc");
+    let WOCRender = document.getElementById("WOCRender");
     const model = models.value();
     const pol = pols.value();
     const mutation_rate = 1/100;
     const crossover_rate = 1/100;
 
     // Cycling through the map and updating creatures 30 update / render
-    for (let i = 0; i < 50; i++) { 
-        world.update(model, pol, mutation_rate, crossover_rate); 
+    var startDateCalc = new Date(); 
+    for (let i = 0; i < 30; i++) { 
+      world.update(model, pol, mutation_rate, crossover_rate); 
     }
-    // world.update(model, pol, mutation_rate, crossover_rate); 
+    var endDateCalc   = new Date();
+
+    var startDate = new Date();
     world.render();
+    var endDate   = new Date();
 
     // Reporting
     var species = [];
@@ -54,54 +64,70 @@ function draw() {
     var loc_a_pop = new Array();
     var loc_b_pop = new Array();
     var loc_c_pop = new Array();
-    var loc_z_pop = new Array();   
+    var loc_z_pop = new Array();
+    var loc_e_pop = new Array();
     for (var i = 0; i <= 255; i++) {
         loc_a_pop[i] = 0;
         loc_b_pop[i] = 0;
         loc_c_pop[i] = 0;
         loc_z_pop[i] = 0;
+        loc_e_pop[i] = 0;
     }
     for ( let i = 0; i < width; i++){ 
         for (let j = 0; j < height; j++){ 
+            // loc_e_pop[world.pool[i][j].fitness]++;
             if (world.pool[i][j].fitness > 0) {
                 creatures += 1;
-                current_fitness = world.pool[i][j].fitness;
-                current_generation = world.pool[i][j].generation;
                 loc_a_pop[world.pool[i][j].locA]++;
                 loc_b_pop[world.pool[i][j].locB]++;
                 loc_c_pop[world.pool[i][j].locC]++;
                 loc_z_pop[world.pool[i][j].locZ]++;
+                loc_e_pop[world.pool[i][j].fitness]++;
                 var my_gene = world.pool[i][j].locA + '-' + world.pool[i][j].locB + '-' + world.pool[i][j].locC;
                 if (typeof species[my_gene] === 'undefined' || !species[my_gene]) {
                     species[my_gene] = 1;
                 } else {
                     species[my_gene]++;
                 }
-            }
-
+            } 
         }
     }
-    WOCModel.innerHTML = model;
+
+    let models_text = [];
+    let seeing = ['AA', 'AB', 'AC', 'BB', 'BC', 'CC']; // These are the options for seen/not seen
+    let eating = ['!AA', '!AB', '!AC', '!BB', '!BC', '!CC']; // These are the options for friend/foe
+    // It appears that there are 36 models although some of them are redundant, like [simAA, oppBB] vs [simBB, oppCC], or senseless, like [simAA, oppAA]
+    for (let k = 0; k < seeing.length; k++) {
+      for (let l = 0; l < eating.length; l++) {
+          models_text.push([seeing[k], eating[l]])
+      }
+    }
+
+    WOCModel.innerHTML = model + ": " + models_text[model];
     if (pol == 0) {WOCPol.innerHTML = '+';} else {WOCPol.innerHTML = '-';}
     WOCCreatures.innerHTML = creatures;
-    update_canvas('locA', loc_a_pop);
-    update_canvas('locB', loc_b_pop);   
-    update_canvas('locC', loc_c_pop);   
-    update_canvas('locZ', loc_z_pop);
+    WOCCalc.innerHTML = (endDateCalc.getTime() - startDateCalc.getTime());
+    WOCRender.innerHTML = (endDate.getTime() - startDate.getTime());
+
+    update_canvas('locA', loc_a_pop, 'rgba(150,0,0,0.9)');
+    update_canvas('locB', loc_b_pop, 'rgba(0,150,0,0.9)');   
+    update_canvas('locC', loc_c_pop, 'rgba(0,0,150,0.9)');   
+    update_canvas('locZ', loc_z_pop, 'rgba(0,0,0,0.9)');
+    update_canvas('Energy', loc_e_pop, 'rgba(0,0,0,0.7)');
     if (creatures < 1) { noLoop(); }
 }
 
-function update_canvas(gene, frequency) {
+function update_canvas(gene, frequency, filling) {
     // let max_value = frequency.reduce(function(a, b) { return Math.max(a, b); });
     let max_value = frequency.reduce((a, b) => Math.max(a, b));
     let canvas_gene = document.getElementById(gene+'Canvas').getContext("2d");
     canvas_gene.clearRect(0, 0, 255, 255);
     for (let i = 0; i <= 255; i++) {
-        canvas_gene.fillStyle = 'rgba(0,0,0,0.8)';
+        canvas_gene.fillStyle = filling;
         canvas_gene.beginPath();
         canvas_gene.moveTo(i, 100);
         canvas_gene.lineTo(i, 100-Math.ceil(frequency[i]/max_value*100));
-        canvas_gene.strokeStyle = 'rgba(0,0,0,0.8)';
+        canvas_gene.strokeStyle = filling;
         canvas_gene.stroke();        
     }
 }
@@ -114,7 +140,7 @@ function World() {
         this.pool[x][y] = new CreatureNull(x, y);
       }
     }
-    this.pool[48][32] = new CreatureFix(48, 32);
+    this.pool[width/2][height/2] = new CreatureFix(width/2, height/2);
 }
 
 function CreatureNull(x, y) {
@@ -172,7 +198,7 @@ function CreatureX(x, y, parent, mutation_rate) {
 
   // Mutation
   if (Math.random() < mutation_rate) {
-  let mutation = Math.floor(Math.random() * 6)
+  let mutation = Math.floor(Math.random() * 8)
     if (mutation == 0) {
       a += Math.ceil(Math.random() * mut_range);
       if (a > 255) { a -= 256; }
@@ -191,6 +217,12 @@ function CreatureX(x, y, parent, mutation_rate) {
     } else if (mutation == 5) {
       c -= Math.ceil(Math.random() * mut_range);
       if (c < 0) { c += 256; }
+    } else if (mutation == 6) {
+      z += Math.ceil(Math.random() * mut_range);
+      if (z > 255) { z -= 256; }
+    } else if (mutation == 7) {
+      z -= Math.ceil(Math.random() * mut_range);
+      if (z < 0) { z += 256; }
     }
   }
 
@@ -206,7 +238,7 @@ function CreatureX(x, y, parent, mutation_rate) {
   
 function CreatureM(x, y, parent, partner, mutation_rate, crossover_rate) {
   a = selectAllele(parent.locA, partner.locA);
-  b = selectAllele(parent.locB, partner.locA);
+  b = selectAllele(parent.locB, partner.locB);
   c = selectAllele(parent.locC, partner.locC);
   z = selectAllele(parent.locZ, partner.locZ);
   e = Math.floor(parent.fitness / 2);
@@ -243,23 +275,28 @@ function CreatureM(x, y, parent, partner, mutation_rate, crossover_rate) {
     }
   }
 
-  // Crossover
-  if (Math.random() < crossover_rate) {
-    let crossover = Math.floor(Math.random() * 3);
-    if (crossover == 0) {
-        let t = a;
-        a = b;
-        b = t;        
-    } else if (crossover == 1) {
-        var t = a;
-        a = c;
-        c = t;           
-    } else if (crossover == 2) {
-        var t = b;
-        b = c;
-        c = t;    
-    }
-  }  
+  // const mutations = [
+  //   { var: a, op: 1 },
+  //   { var: a, op: -1 },
+  //   { var: b, op: 1 },
+  //   { var: b, op: -1 },
+  //   { var: c, op: 1 },
+  //   { var: c, op: -1 },
+  //   { var: z, op: 1 },
+  //   { var: z, op: -1 },
+  // ];
+  
+  // const { var: target, op } = mutations[mutation];
+  // target += Math.ceil(Math.random() * mut_range) * op;
+  // if (target > 255) { target -= 256; }
+  // else if (target < 0) { target += 256; }
+
+  // Crossover, if triggered swaps random two genes of the available six possible combinations
+  // if (Math.random() < crossover_rate) {
+  //   let swaps = [[a, b], [a, c], [a, z], [b, c], [b, z], [c, z]];
+  //   swaps[Math.floor(Math.random() * swaps.length)].reverse();
+  // }
+
 
   // Finally the Offspring
   this.geo = createVector(x, y);
@@ -270,7 +307,7 @@ function CreatureM(x, y, parent, partner, mutation_rate, crossover_rate) {
   this.fitness = e;
   this.generation = 0;
 }
-  
+
 function remove_multiplications(offsprings) {
     // Sets fitness to 0 for offsprings occupying the same location
     for (let i = 0; i < offsprings.length; i++) {
@@ -294,41 +331,88 @@ function remove_multiplications(offsprings) {
     }
     return offsprings;
 }
-  
+
 World.prototype.update = function(model, pol, mutation_rate, crossover_rate) {
   const newPool = this.pool.map(row => row.map(item => ({...item})));
   const shuffled = shuffledIndexOfArray(newPool)
 
   let offsprings = [];
-  let polarization = 1;
 
   for (const [x, y] of shuffled) {
     if (this.pool[x][y].fitness > 0) {
       let fitness = this.pool[x][y].fitness;
       let generation = this.pool[x][y].generation + 1;
-      let allNeighbours = getNeighbours(this.pool, x, y, model);
+      let allNeighbors = getNeighbors(this.pool, x, y);
+      let friends = 0;
+      let foes = 0;
+      let unknowns = 0;
 
-      if (pol == 1) { polarization = -1; } else { polarization = 1; }
-      fitness = fitness + allNeighbours[1].length * polarization;
-      fitness = fitness + allNeighbours[2].length * polarization * (-1);
-      fitness = fitness + allNeighbours[3].length;
+      // console.log(models[model])
+      // value your neighbors
+      for (let i = 0; i < allNeighbors[0].length; i++) {
+        let neighbor = allNeighbors[0][i];
+
+        let AA = similar(this.pool[x][y].locA, neighbor.locA)
+        let AB = similar(this.pool[x][y].locA, neighbor.locB)
+        let AC = similar(this.pool[x][y].locA, neighbor.locC)
+        let BB = similar(this.pool[x][y].locB, neighbor.locB)
+        let BC = similar(this.pool[x][y].locB, neighbor.locC)
+        let CC = similar(this.pool[x][y].locC, neighbor.locC)        
+
+        if (model == 0) { AA ? (AA ? friends++ : foes++) : unknowns++ }
+        if (model == 1) { AA ? (AB ? friends++ : foes++) : unknowns++ }
+        if (model == 2) { AA ? (AC ? friends++ : foes++) : unknowns++ }
+        if (model == 3) { AA ? (BB ? friends++ : foes++) : unknowns++ }
+        if (model == 4) { AA ? (BC ? friends++ : foes++) : unknowns++ }
+        if (model == 5) { AA ? (CC ? friends++ : foes++) : unknowns++ }
+
+        if (model == 6) { AB ? (AA ? friends++ : foes++) : unknowns++ }
+        if (model == 7) { AB ? (AB ? friends++ : foes++) : unknowns++ }
+        if (model == 8) { AB ? (AC ? friends++ : foes++) : unknowns++ }
+        if (model == 9) { AB ? (BB ? friends++ : foes++) : unknowns++ }
+        if (model == 10) { AB ? (BC ? friends++ : foes++) : unknowns++ }
+        if (model == 11) { AB ? (CC ? friends++ : foes++) : unknowns++ }
+
+        if (model == 12) { AC ? (AA ? friends++ : foes++) : unknowns++ }
+        if (model == 13) { AC ? (AB ? friends++ : foes++) : unknowns++ }
+        if (model == 14) { AC ? (AC ? friends++ : foes++) : unknowns++ }
+        if (model == 15) { AC ? (BB ? friends++ : foes++) : unknowns++ }
+        if (model == 16) { AC ? (BC ? friends++ : foes++) : unknowns++ }
+        if (model == 17) { AC ? (CC ? friends++ : foes++) : unknowns++ }
+
+        if (model == 18) { BB ? (AA ? friends++ : foes++) : unknowns++ }
+        if (model == 19) { BB ? (AB ? friends++ : foes++) : unknowns++ }
+        if (model == 20) { BB ? (AC ? friends++ : foes++) : unknowns++ }
+        if (model == 21) { BB ? (BB ? friends++ : foes++) : unknowns++ }
+        if (model == 22) { BB ? (BC ? friends++ : foes++) : unknowns++ }
+        if (model == 23) { BB ? (CC ? friends++ : foes++) : unknowns++ }
+
+        if (model == 24) { BC ? (AA ? friends++ : foes++) : unknowns++ }
+        if (model == 25) { BC ? (AB ? friends++ : foes++) : unknowns++ }
+        if (model == 26) { BC ? (AC ? friends++ : foes++) : unknowns++ }
+        if (model == 27) { BC ? (BB ? friends++ : foes++) : unknowns++ }
+        if (model == 28) { BC ? (BC ? friends++ : foes++) : unknowns++ }
+        if (model == 29) { BC ? (CC ? friends++ : foes++) : unknowns++ }
+
+        if (model == 30) { CC ? (AA ? friends++ : foes++) : unknowns++ }
+        if (model == 31) { CC ? (AB ? friends++ : foes++) : unknowns++ }
+        if (model == 33) { CC ? (AC ? friends++ : foes++) : unknowns++ }
+        if (model == 33) { CC ? (BB ? friends++ : foes++) : unknowns++ }
+        if (model == 34) { CC ? (BC ? friends++ : foes++) : unknowns++ }
+        if (model == 35) { CC ? (CC ? friends++ : foes++) : unknowns++ }
+      }
+      let polarization = (pol == 0) ? 1: -1;
+      fitness -= 0;                                                 
+      fitness = fitness - unknowns;  
+      fitness = fitness + friends * polarization;
+      fitness = fitness - foes * polarization;
+      fitness = fitness + allNeighbors[1].length;
       
-      if (fitness > fitness_mit) {
-        if (allNeighbours[1].length + allNeighbours[2].length < 7) {
-          if (allNeighbours[3].length>0){
-            let newCell = random(allNeighbours[3]);
-            if (newCell){
-              if (allNeighbours[2].length > 0) {
-                // Meiosis
-                let partner = random(allNeighbours[2]);
-                offsprings.push(new CreatureM(newCell.geo.x, newCell.geo.y, newPool[x][y], partner, mutation_rate, crossover_rate));
-              } else {
-                // Mitosis, no cross over
-                offsprings.push(new CreatureX(newCell.geo.x, newCell.geo.y, newPool[x][y], mutation_rate));
-              }
-            }
-          }
-          fitness = fitness - Math.floor(fitness/2);
+      if (fitness > fitness_mit && allNeighbors[1].length > 0) {
+        let newCell = random(allNeighbors[1]);
+        if (newCell){
+          offsprings.push(new CreatureX(newCell.geo.x, newCell.geo.y, newPool[x][y], mutation_rate));
+          fitness = Math.floor(fitness/2);
         }
       }
       if (fitness > 255) { fitness = 255; }
@@ -344,7 +428,8 @@ World.prototype.update = function(model, pol, mutation_rate, crossover_rate) {
     // Only place new offspring if the spot is empty
     if (offsprings[i].fitness > 0 && newPool[offsprings[i].geo.x][offsprings[i].geo.y].fitness == 0) {
       newPool[offsprings[i].geo.x][offsprings[i].geo.y] = new CreatureO(offsprings[i].geo.x, offsprings[i].geo.y, offsprings[i].locA, offsprings[i].locB, offsprings[i].locC, offsprings[i].locZ, offsprings[i].fitness, 0);
-    } else {
+    } 
+    else {
       newPool[offsprings[i].geo.x][offsprings[i].geo.y].fitness += offsprings[i].fitness;
       if (newPool[offsprings[i].geo.x][offsprings[i].geo.y].fitness > 255) {
         newPool[offsprings[i].geo.x][offsprings[i].geo.y].fitness = 255;
@@ -358,78 +443,21 @@ World.prototype.update = function(model, pol, mutation_rate, crossover_rate) {
 World.prototype.render = function() {
   // Model defines colors to show
   background(0);
-  let red = 0;
-  let green = 0;
-  let blue = 0;
-  let opacity = 0;
-
-  // How about average color?
 
   for (let x = 0; x < width; x ++) {
     for (let y = 0; y < height; y ++) {
       let cell = this.pool[x][y];
-
-      red = cell.locA;
-      green = cell.locB;
-      blue = cell.locC;
-      opacity = cell.fitness; //129 + Math.floor(cell.fitness / 2);
-            
       if (cell.fitness > 0) {
-        fill(red, green, blue, opacity);
-        stroke(0);
-        square(x*steps, y*steps, steps);
-      } else {
-        let colors = getNeighboursColors(this.pool, x, y);
-        red = colors[0];
-        green = colors[1];
-        blue = colors[2];
-        opacity = colors[3];
-        // fill(red, green, blue, 255);
-        fill(0, 0, 0, 255);
-        stroke(0);
+        fill(cell.locA, cell.locB, cell.locC, cell.fitness);
         square(x*steps, y*steps, steps);
       }
     }
   }
-}
-
-function getNeighboursColors(pool, x, y) {
-  let Red = 0;
-  let Green = 0;
-  let Blue = 0;
-  let Opacity = 0;
-  for (let i = -1; i <= 1; i++) {
-    for (let j = -1; j <= 1; j++) {
-      let new_x = x + i;
-      let new_y = y + j;
-      
-      // Get from Flat World to Globe
-      if (new_x < 0) {new_x = width + new_x}
-      if (new_y < 0) {new_y = height + new_y}
-      if (new_x >= width) {new_x = new_x - width}
-      if (new_y >= height) {new_y = new_y - height}
-
-      if (pool[new_x][new_y].fitness > 0){
-        Red += pool[new_x][new_y].locA
-        Green += pool[new_x][new_y].locB
-        Blue += pool[new_x][new_y].locC
-        Opacity += pool[new_x][new_y].fitness
-      }
-    }
-  }
-  Red = Math.floor(Red/8);
-  Green = Math.floor(Green/8);
-  Blue = Math.floor(Blue/8);
-  Opacity = Math.floor(Opacity/8);
-
-  return 0, 0, 0, 0
 }
   
-function getNeighbours(pool, x, y, model) {
+function getNeighbors(pool, x, y) {
   // Returns neighbors, friends, foes and empty cells for the creature at x/y. 
   let neighbors = [];
-  let friends = [];
-  let foes = [];
   let empties = [];
   for (let i = -1; i <= 1; i++) {
     for (let j = -1; j <= 1; j++) {
@@ -443,49 +471,15 @@ function getNeighbours(pool, x, y, model) {
         if (new_x >= width) {new_x = new_x - width}
         if (new_y >= height) {new_y = new_y - height}
 
-        if (new_x >= 0 && new_x < width && new_y >= 0 && new_y < height) {     
-          if (pool[new_x][new_y].fitness > 0) {
-            neighbors.push(pool[new_x][new_y]);
-            if (model == 1) {
-              if (similar(pool[x][y].locC, pool[new_x][new_y].locC)) {
-                friends.push(pool[new_x][new_y]);
-              } else if (opposite(pool[x][y].locC, pool[new_x][new_y].locC)) {
-                foes.push(pool[new_x][new_y]);
-              }
-            } else if (model == 2) {
-              if (similar(pool[x][y].locA, pool[new_x][new_y].locA)) {
-                if (similar(pool[x][y].locC, pool[new_x][new_y].locC)) {
-                  friends.push(pool[new_x][new_y]);
-                } else if (opposite(pool[x][y].locC, pool[new_x][new_y].locC)) {
-                  foes.push(pool[new_x][new_y]);
-                }
-              }
-            } else if (model == 3) {
-              if (similar(pool[x][y].locA, pool[new_x][new_y].locB)) {
-                if (similar(pool[x][y].locC, pool[new_x][new_y].locC)) {
-                  friends.push(pool[new_x][new_y]);
-                } else if (opposite(pool[x][y].locC, pool[new_x][new_y].locC)) {
-                  foes.push(pool[new_x][new_y]);
-                }
-              }
-            } else if (model == 4) {
-              if (similar(pool[x][y].locA, pool[new_x][new_y].locC)) {
-                if (similar(pool[x][y].locC, pool[new_x][new_y].locC)) {
-                  friends.push(pool[new_x][new_y]);
-                } else if (opposite(pool[x][y].locC, pool[new_x][new_y].locC)) {
-                  foes.push(pool[new_x][new_y]);
-                }
-              }
-            }
-          }
-          else {
-            empties.push(pool[new_x][new_y]);
-          }
-        } // why do I get values 96 and 64
+        if (pool[new_x][new_y].fitness > 0) {
+          neighbors.push(pool[new_x][new_y]);
+        } else {
+          empties.push(pool[new_x][new_y]);
+        }
       }
     }
   }
-  return [neighbors, friends, foes, empties];
+  return [neighbors, empties];
 }
   
 function selectAllele(allele1, allele2) {
